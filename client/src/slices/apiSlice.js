5d0e40ba255284915937bb8e7296039ebe0a029e
odunzechidubem <odunzechidubem73@gmail.com>
2025-08-02 22:27:32 +0100

    feat: Implement production environment variables

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,13 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+const baseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_BACKEND_URL,
+  credentials: 'include',
+});
 
 export const apiSlice = createApi({
   baseQuery,
   tagTypes: [
     'User', 'Users', 'Course', 'AllCourses', 'MyCourses', 'Assignment',
     'Submissions', 'Announcements', 'Notifications', 'Settings', 'FooterLink',
-    'Article', // <-- ADD TAG
+    'Article',
   ],
   endpoints: (builder) => ({}),
 });
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,26 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [ { src: 'node_modules/pdfjs-dist/build/pdf.worker.min.js', dest: '' } ]
-    })
-  ],
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        // --- THIS IS THE CRITICAL CHANGE ---
-        target: 'https://lectern-usqo.onrender.com', // <-- Use your live Render URL
+        target: 'http://localhost:5000',
         changeOrigin: true,
-      },
-      '/pdf-proxy': {
-        target: 'https://res.cloudinary.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/pdf-proxy/, ''),
       },
     },
   },
-})
+});
