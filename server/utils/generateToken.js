6a56832ded55ccade4ab87b68d0c65a3703c5145
odunzechidubem <odunzechidubem73@gmail.com>
2025-08-10 06:35:45 +0100

    some fixes on the Header

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     // --- THIS IS THE CHANGE ---
-    expiresIn: '3m', // Set the JWT itself to expire in 3 minutes
+    expiresIn: '5m', // Set the JWT itself to expire in 5 minutes
   });
 
   res.cookie('jwt', token, {
@@ -11,9 +11,9 @@ const generateToken = (res, userId) => {
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'strict',
     // --- THIS IS THE CHANGE ---
-    // maxAge is in milliseconds. This sets the browser cookie to expire in 3 minutes.
-    // 3 minutes * 60 seconds/minute * 1000 milliseconds/second
-    maxAge: 3 * 60 * 1000,
+    // maxAge is in milliseconds. This sets the browser cookie to expire in 5 minutes.
+    // 5 minutes * 60 seconds/minute * 1000 milliseconds/second
+    maxAge: 5 * 60 * 1000,
   });
 };
 
