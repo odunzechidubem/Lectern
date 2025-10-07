c4b61d9e359a92b0cd02887cf8e4f81087cbdccd
odunzechidubem <odunzechidubem73@gmail.com>
2025-10-07 06:19:11 +0100

    worked on notifications

--- server/routes/notificationRoutes.js
+++ server/routes/notificationRoutes.js
@@ -1,19 +1,14 @@
 import express from 'express';
+const router = express.Router();
 import {
   getMyNotifications,
   markNotificationsAsRead,
   markOneAsRead,
-  createAndEmitNotification,
 } from '../controllers/notificationController.js';
 import { protect, isUser } from '../middleware/authMiddleware.js';
 
-const router = express.Router();
-
-router.route('/')
-  .get(protect, isUser, getMyNotifications)
-  .post(protect, createAndEmitNotification);
-
+router.route('/').get(protect, isUser, getMyNotifications);
 router.route('/mark-read').put(protect, isUser, markNotificationsAsRead);
 router.route('/:id/mark-read').put(protect, isUser, markOneAsRead);
 
-export default router;
+export default router; 
